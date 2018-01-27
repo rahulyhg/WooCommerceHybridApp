27126e7ed70987966c5e4c0ab3d14c55dc7a4984
singhsanket143 <singhsanket143@gmail.com>
2018-01-28 01:09:36 +0530

    infinite scroll added with toast message

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { NavController, Slides } from 'ionic-angular';
+import { NavController, Slides, ToastController } from 'ionic-angular';
 import * as WC from 'woocommerce-api';
 @Component({
   selector: 'page-home',
@@ -10,16 +10,16 @@ export class HomePage {
   WooCommerce: any;
   products: any[];
   moreProducts: any[];
-  page: number = 2;
+  page: number = 1;
   @ViewChild('productSlides') productSlides: Slides;
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
     this.WooCommerce = WC({
       url: 'http://localhost/wordpress/',
       origin: "*",
       consumerKey: 'ck_077957b6f9044e67200d151f7ee5fb9e3e272938',
       consumerSecret: 'cs_be94e6c18a7e02327419d7be686c25abc2636e01',
     });
-    this.loadMoreProducts();
+    this.loadMoreProducts(null);
     this.WooCommerce.getAsync('products').then( (data) => {
       console.log(JSON.parse(data.body));
       this.products = JSON.parse(data.body).products;
@@ -37,10 +37,27 @@ export class HomePage {
     },3000);
   }
 
-  loadMoreProducts() {
+  loadMoreProducts(event) {
+    if(event == null) {
+      this.page = 1;
+      this.moreProducts = [];
+    } else {
+      this.page++;
+    }
     this.WooCommerce.getAsync('products?page='+this.page).then( (data) => {
       console.log(JSON.parse(data.body));
-      this.moreProducts = JSON.parse(data.body).products;
+      this.moreProducts = this.moreProducts.concat(JSON.parse(data.body).products);
+      if(event!=null){
+        event.complete();
+      }
+      if(JSON.parse(data.body).products.length < 10){
+
+        event.enable(false);
+        this.toastCtrl.create({
+          message: "No More Products!!!",
+          duration: 5000
+        }).present();
+      }
     }, (err) => {
       console.log(err);
     })
