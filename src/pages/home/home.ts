e2205cd7fe6cb234cb86e132ee307fc1d516d2e0
singhsanket143 <singhsanket143@gmail.com>
2018-01-28 00:57:04 +0530

    further list added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,13 +9,17 @@ export class HomePage {
 
   WooCommerce: any;
   products: any[];
+  moreProducts: any[];
+  page: number = 2;
   @ViewChild('productSlides') productSlides: Slides;
   constructor(public navCtrl: NavController) {
     this.WooCommerce = WC({
       url: 'http://localhost/wordpress/',
+      origin: "*",
       consumerKey: 'ck_077957b6f9044e67200d151f7ee5fb9e3e272938',
       consumerSecret: 'cs_be94e6c18a7e02327419d7be686c25abc2636e01',
     });
+    this.loadMoreProducts();
     this.WooCommerce.getAsync('products').then( (data) => {
       console.log(JSON.parse(data.body));
       this.products = JSON.parse(data.body).products;
@@ -31,6 +35,15 @@ export class HomePage {
       }
       this.productSlides.slideNext();
     },3000);
+  }
+
+  loadMoreProducts() {
+    this.WooCommerce.getAsync('products?page='+this.page).then( (data) => {
+      console.log(JSON.parse(data.body));
+      this.moreProducts = JSON.parse(data.body).products;
+    }, (err) => {
+      console.log(err);
+    })
   }
 
 }
